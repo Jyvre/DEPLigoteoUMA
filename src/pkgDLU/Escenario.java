7ef8bf7edbfd978f8b54725833fd1364a96d0255
CYSTORBOOKV2\Javier <thej96fa@gmail.com>
2018-04-18 12:39:16 +0200

    v0.1.0
    Added the last scene and finished, times are better and bugs fixes

--- src/pkgDLU/Escenario.java
+++ src/pkgDLU/Escenario.java
@@ -48,11 +48,11 @@ public class Escenario {
 
     public void firstScene(Scanner sc) throws  InterruptedException{
 
-        System.out.println("Escena 1/3: \n\n" +
+        System.out.println("\nEscena 1/3: \n\n" +
                 "Tu bello corcel de nombre " + cono.getName() + ", se acerca a ti con su apuesta mirada\n" +
                 "en un intento desesperado de llamar tu atención. Pero tan solo desea ser tu amigo por ahora ¿Qué\n" +
                 "haces para impresionarle?\n");
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         System.out.println("Tus opciones son:\n" +
                 "Pulsa 1: Saludas con un seductor 'hola' y esperas que él inicie la conversación\n" +
                 "Pulsa 2: Empiezas a hablar sobre tus aficiones y tu vida a la par que te interesas por las suyas\n" +
@@ -101,7 +101,7 @@ public class Escenario {
                 cono.getName() + ", empieza a hablar de politica, es un tema peliagudo y resuslta que su\n" +
                 "idologia es rara de cojones y puede llevar a conflicto, ya no solo contigo sino\n" +
                 "con aquel que le redoe. ¿Cómo actuas?\n");
-        Thread.sleep(3000);
+        Thread.sleep(7000);
         System.out.println("Tus opciones son:\n" +
                 "Pulsa 1: Tratas de cambiar de tema sin darle imporancia\n" +
                 "Pulsa 2: Hablas de tu ideologia y le discutes todo lo discutible\n" +
@@ -117,13 +117,13 @@ public class Escenario {
         }
         Thread.sleep(3000);
         switch (a){
-            case 1: System.out.println(cono.getName() + " la conversación vuelve a fluir y todo parece ir bien.\n");
+            case 1: System.out.println("La conversación vuelve a fluir y todo parece ir bien.\n");
                 cono.ponKarma(1);
                 break;
-            case 2: System.out.println(cono.getName() + " la conversación se vuelve incomoda, discutis... ¡Viva Franco!\n");
+            case 2: System.out.println("La conversación se vuelve incomoda, discutis... ¡Viva Franco!\n");
                 cono.quitaKarma(2);
                 break;
-            case 3: System.out.println(cono.getName() + " se cruzan las miradas y deja de hablar de política, parece que todo fluye," +
+            case 3: System.out.println(cono.getName() + " te devuelve las miradas y deja de hablar de política, parece que todo fluye," +
                     "pero al final estás demasiado nerviosa para besarle, cambia de conversación\n");
                 cono.ponKarma(3);
                 break;
@@ -145,8 +145,65 @@ public class Escenario {
 
     }
 
-    private void menuDeRecursos() throws InterruptedException{
+    public void thirdScene(Scanner sc) throws  InterruptedException{
         Thread.sleep(3000);
+        System.out.println("\n\nEscena 3/3: \n\n" +
+                cono.getName() + " se levanta. Finalmente es la hora de marchar, pero antes de mediar palabra espera una ultima\n" +
+                "reacción tuya. ¿Qué haces?\n");
+        Thread.sleep(7000);
+        System.out.println("Tus opciones son:\n" +
+                "Pulsa 1: Tratas de resaltar alguna buena conversación y te despides cordialmente\n" +
+                "Pulsa 2: Le pisas un pie hasta ver que pone los ojos en blanco y sales corriendo\n" +
+                "Pulsa 3: Le besas alocadamente\n" +
+                "Pulsa 4: Te pones de pie junto a él, le miras a los ojos y confiesas que sientes algo por él\n" +
+                "Pulsa 5: Le das un abrazo amistoso y esperas volver a verle algún día");
+        int a = sc.nextInt();
+        if(a == 0){
+            menuDeRecursos();
+            Thread.sleep(3000);
+            System.out.println("Y ahora la escena, pulsa el numero que consideres");
+            a = sc.nextInt();
+        }
+        Thread.sleep(3000);
+        switch (a){
+            case 1: System.out.println(cono.getName() + " esboza una sonrisa y se marcha despreocupado.\n");
+                cono.ponKarma(1);
+                break;
+            case 2: System.out.println(cono.getName() + " grita maldiciones con tu nomrbe, tal vez no haya sido una buena idea...\n");
+                cono.quitaKarma(6);
+                break;
+            case 3: System.out.println(cono.getName() + " se sorprende por el beso, se ruboriza y se va.\n");
+                cono.ponKarma(3);
+                break;
+            case 4: System.out.println(cono.getName() + " se sorprende, rebusca en sus sentimientos y empieza a poner muecas extrañas,\n" +
+                    "Se va un tanto confuso.\n");
+                cono.ponKarma(2);
+                break;
+            case 5: System.out.println(cono.getName() + " te devuelve el abrazo y se marcha sonriente.\n");
+                break;
+            default: System.out.println("Hay un error en el juego");
+                break;
+        }
+
+        Thread.sleep(3000);
+        System.out.println("Todo llega a su fin, al igual que esta escena. Ha sido cuanto menos curioso, una cita de verdad y no como\n" +
+                "solía ser conocerse en LigoteosUMA. " + cono.getName() + " se va y tiene un claro pensamiento hacia ti.\n" +
+                "Leamosle la mente...\n\n");
+        Thread.sleep(6000);
+        System.out.println("Accediendo a la mente de " + cono.getName()  + "...");
+        Thread.sleep(1000);
+        System.out.println("Intentando acceder con martillo...");
+        Thread.sleep(500);
+        System.out.println("...");
+        Thread.sleep(300);
+        System.out.println(cono.solución() + "\n");
+        Thread.sleep(5000);
+        System.out.println("Tu puntuación ha sido de " + cono.getKarma() + " sobre 10");
+
+    }
+
+    private void menuDeRecursos() throws InterruptedException{
+        Thread.sleep(1500);
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Qué recurso quieres usar? \n" +  conr.getRecursos().toString() + " \n" +
                 "Para usar uno de ellos teclealo, no importan mayusculas o minisculas");
--- src/pkgDLU/Run.java
+++ src/pkgDLU/Run.java
@@ -11,22 +11,24 @@ public class Run {
                             "se ha dignado a ligar contigo en persona.\n" +
                 "Cuentame, ¿cómo se llama el mozo?");
         String nomCO = sc.nextLine();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
         System.out.println("Buena elección. ¿Y tu nombre cual es?");
         String nomCR = sc.nextLine();
-        Thread.sleep(3000);
+        Thread.sleep(1000);
         System.out.println("Comencemos...\n");
         Escenario esc = new Escenario(nomCO, nomCR);
-        Thread.sleep(3000);
+        Thread.sleep(2500);
         System.out.println("Te voy a conceder estos recursos: " + esc.getRecursos().toString() + "\n" +
                 "Podrás usarlos en cualquier momento pulsando 0.\n");
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         System.out.println("Ademas te doy "+ esc.getConr().getVida() +" escenas para lograrlo.\n" +
                 "Sin más dilación que llegue... la primera escena." );
-        Thread.sleep(3000);
+        Thread.sleep(7000);
         esc.firstScene(sc);
-        Thread.sleep(3000);
+        Thread.sleep(7000);
         esc.secondScene(sc);
+        Thread.sleep(7000);
+        esc.thirdScene(sc);
 
     }
 
